2ad52f48d295fd9685720fcbe615aaae44ab19b0
Allen Wen <acw6923@rit.edu>
2023-05-02 11:31:21 -0500

    moved boxes closer

--- ErrorEstimator/src/error.cxx
+++ ErrorEstimator/src/error.cxx
@@ -9,11 +9,12 @@
 #include <array>
 #include <cassert>
 #include <cmath>
+#include <stdio.h>
 
 namespace ErrorEstimator {
 using namespace Loop;
 
-enum class shape_t { sphere, cube };
+enum class shape_t { sphere, cube, boxes };
 
 template <typename T> constexpr T radius_sphere(const vect<T, dim> &X) {
   using std::sqrt;
@@ -23,6 +24,12 @@ template <typename T> constexpr T radius_sphere(const vect<T, dim> &X) {
 template <typename T> constexpr T radius_cube(const vect<T, dim> &X) {
   using std::abs;
   return maximum(abs(X));
+}
+
+template <typename T> constexpr T radius_boxes(const vect<T, dim> &X, const vect<T, dim> &p1, const vect<T, dim> &p2, const vect<int, dim> &scale) {
+  using std::abs;
+  using std::fmin; 
+  return fmin(maximum(abs(scale * (X - p1))), maximum(abs(scale * (X - p2))));
 }
 
 template <typename T, int CI, int CJ, int CK>
@@ -40,6 +47,8 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_ErrorEstimator_Estimate;
   DECLARE_CCTK_PARAMETERS;
 
+  assert(cctkGH);
+
   const vect<int, dim> ones{1, 1, 1};
   const vect<int, dim> levfac{cctk_levfac[0], cctk_levfac[1], cctk_levfac[2]};
   const vect<int, dim> scalefactors = scale_by_resolution ? levfac : ones;
@@ -48,11 +57,24 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
   const GF3D2layout layout(cctkGH, indextype);
   const GF3D2<CCTK_REAL> regrid_error_(layout, regrid_error);
 
+  // fprintf(stderr, "%d\n", cctkGH);
+  // fprintf(stderr, "%d\n", cctk_iteration);
+  const int sim_iter = cctk_iteration;
+  // const double time = cctk_time;
+  const double freq = 2 * M_PI / 1000;
+  const vect<double, dim> p1{5 * cos(freq * sim_iter), 5 * sin(freq * sim_iter), 0};
+  const vect<double, dim> p2{-5 * cos(freq * sim_iter), -5 * sin(freq * sim_iter), 0};
+  fprintf(stderr, "%20.16e %20.16e %20.16e\n", p1[0], p1[1], p1[2]);
+  // const vect<double, dim> p1{10, 0, 1};
+  // const vect<double, dim> p2{-10, 0, 1};
+
   const shape_t shape = [&]() {
     if (CCTK_EQUALS(region_shape, "sphere"))
       return shape_t::sphere;
     if (CCTK_EQUALS(region_shape, "cube"))
       return shape_t::cube;
+    if (CCTK_EQUALS(region_shape, "boxes"))
+      return shape_t::boxes;
     std::abort();
   }();
 
@@ -66,6 +88,8 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
             return radius_sphere(scalefactors * p.X);
           case shape_t::cube:
             return radius_cube(scalefactors * p.X);
+          case shape_t::boxes:
+            return radius_boxes(p.X, p1, p2, scalefactors);
           default:
             assert(0);
           };
